f10333da452ee62a88f8ee8cf3031ae484cba84e
Willian Antunes <LaramosGalion@users.noreply.github.com>
2023-06-28 21:11:40 -0300

    Update index.js

---
+++ Aula13/index.js
@@ -0,0 +1,25 @@
+let nome = "Um texto";
+//          01234567
+//         -87654321
+
+console.log(nome[4]);
+console.log(nome[8]);
+console.log(nome.charAt(6));
+console.log(nome.concat (" concatenado", " ao texto"));
+console.log(nome.indexOf("x",1));
+console.log(nome.lastIndexOf("x",6 ));
+console.log(nome.match(/[a-z]/g));
+console.log(nome.search(/x/));
+let frase = "O rato roeu a roupa do rei de roma";
+console.log(frase.replace(/r/g,"RR"));
+console.log(frase.length);
+console.log(frase.slice(2,6));
+console.log(frase.slice(-4));
+console.log(frase.substring(frase.length-4));
+console.log(frase.split (" "));
+console.log(frase.toLocaleUpperCase());
+console.log(frase.toLocaleLowerCase());
+
+
+
+
