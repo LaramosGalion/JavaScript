11352e4ac690fafb72fca0020524e6fd15250440
Willian Antunes <LaramosGalion@users.noreply.github.com>
2023-07-05 18:17:50 -0300

    Estudos 04/07

--- Aula16 - Exercicio/script/index.js
+++ Aula16 - Exercicio/script/index.js
@@ -1,10 +1,23 @@
 let num = prompt(`Digite um número: `);
 
+const numeroTitulo = document.getElementById("numero_titulo");
+
+const texto = document.getElementById("texto");
+
 num = Number(num);
 
-document.body.innerHTML = `A raiz quadrada do número é: ${Math.sqrt(num)} <br>`;
-document.body.innerHTML += `${num} é inteiro?: ${Number.isInteger(num)} <br>`;
-document.body.innerHTML += `${num} é NaN?: ${Number.isNaN(num)} <br>`;
-document.body.innerHTML += `${num} arrendondado para baixo é: ${Math.floor(num)} <br>`;
-document.body.innerHTML += `${num} arrendondado para cima é: ${Math.ceil(num)} <br>`;
-document.body.innerHTML += `${num} com duas casas decimais é: ${Number(num.toFixed(2))} <br>`;
+numeroTitulo.innerHTML = num;
+
+texto.innerHTML = ""; 
+
+texto.innerHTML += `<p>A raiz quadrada do número é: ${Math.sqrt(num)}</p>`;
+
+texto.innerHTML += `<p>${num} é inteiro?: ${Number.isInteger(num)}</p>`;
+
+texto.innerHTML += `<p>${num} é NaN?: ${Number.isNaN(num)}</p>`;
+
+texto.innerHTML += `<p>${num} arrendondado para baixo é: ${Math.floor(num)}</p>`;
+
+texto.innerHTML += `<p>${num} arrendondado para cima é: ${Math.ceil(num)}</p>`;
+
+texto.innerHTML += `<p>${num} com duas casas decimais é: ${num.toFixed(2)}</p>`; 
