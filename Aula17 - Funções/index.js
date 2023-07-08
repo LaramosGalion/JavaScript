421818f1b55d30d5956ed968bb71747a97148d68
Willian Antunes <LaramosGalion@users.noreply.github.com>
2023-07-08 20:34:05 -0300

    Create index.js

---
+++ Aula17 - Funções/index.js
@@ -0,0 +1,45 @@
+function saudacao(nome) {
+    return `Olá ${nome}`;
+    
+};
+
+saudacao("willian");
+console.log(saudacao("maria"));
+const variavel = saudacao("pedro");
+console.log(variavel);
+
+function soma (num1, num2){
+    console.log("Olá Mundo")
+    return num1 + num2;
+    console.log("Olá Mundo") 
+}
+
+function sum (num1 = 15, num2 = 5){
+    let resultado = num1 + num2;
+    return resultado;
+}
+
+console.log(soma(15, 15), sum(20,20));
+
+const resultado = sum(40, 40);
+ 
+console.log(resultado);
+
+const teste = sum();
+
+console.log(teste);
+
+const raiz = function (numero){
+     return numero ** 0.5;
+};
+console.log(raiz(81)); 
+
+const raizz = (numero) =>{
+    return numero ** 0.5;
+};
+
+console.log(raizz(225));
+
+const square = n => n ** 0.5;
+
+console.log(square(400));
