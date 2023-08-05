c65b722f8ff39fcdc0921cb627d589a55cfa6ef7
Willian Antunes <LaramosGalion@users.noreply.github.com>
2023-08-04 21:58:12 -0300

    att

--- Aula26 - Exercicio de imc/modelo/assets/js/main.js
+++ Aula26 - Exercicio de imc/modelo/assets/js/main.js
@@ -1,47 +1,60 @@
-    
-function imc(){
+
+function imc() {
 
     const formulario = document.querySelector("#form");
 
-    const resultado = document.querySelector("#resultado");
+    const resultado = document.querySelector("#resultado_section");
 
-    function enviar(evento){
-        evento.preventDefault();
+    function enviar(evento) {
+        evento.preventDefault(); // Prevenção do evento 
 
-        const peso = formulario.querySelector(".peso");
-;
-        const altura = formulario.querySelector(".altura");
+        console.log("prevenção do evento");
 
-        if(altura.value == 0 || peso.value == 0){
-            return resultado.innerHTML = `<p>Peso ou altura invalidos</p>`;
+        const peso = Number(formulario.querySelector(".peso").value);
+
+        const altura = Number(formulario.querySelector(".altura").value);
+
+        console.log(typeof (altura), typeof (peso)); // Verificação do tipo
+
+        if (altura == 0 || peso == 0) {
+            resultado_section.style.background = "red";
+            return resultado.innerHTML = `<p>Peso ou altura invalidos (Não é possivel utilizar zero como parâmetro)</p>`;
+
+        } else if (Number.isNaN(altura) || Number.isNaN(peso)) {
+            resultado_section.style.background = "red";
+            return resultado.innerHTML = `<p>Peso ou altura invalidos (Digite apenas números)</p>`;
+
+        }else{
+            resultado_section.style.background = "var(--corBase)";
         }
 
-        const imc = (peso.value / (altura.value**2)).toFixed(2);
+        const imc = (peso / (altura ** 2)).toFixed(2);
 
-        if (imc < 18.5){
-            console.log(1);
-             return resultado.innerHTML = `<p>Seu IMC é de ${imc} (Abaixo do peso)</p>`;
-        }else if(imc >= 18.5 && imc < 25){
-            console.log(2);
+        if (imc < 18.5) {
+            console.log(1); // Verificação do resultado 
+            return resultado.innerHTML = `<p>Seu IMC é de ${imc} (Abaixo do peso)</p>`;
+        } else if (imc >= 18.5 && imc < 25) {
+            console.log(2); // Verificação do resultado
             return resultado.innerHTML = `<p>Seu IMC é de ${imc} (Peso normal)</p>`;
-        }else if(imc >= 25 && imc < 30 ){
-            console.log(3);
+        } else if (imc >= 25 && imc < 30) {
+            console.log(3); // Verificação do resultado
             return resultado.innerHTML = `<p>Seu IMC é de ${imc} (Sobrepeso)</p>`;
-        }else if(imc >= 30 && imc < 35 ){
-            console.log(4);
+        } else if (imc >= 30 && imc < 35) {
+            console.log(4); // Verificação do resultado
             return resultado.innerHTML = `<p>Seu IMC é de ${imc} (Obesidade grau 1)</p>`;
-        }else if(imc >= 35 && imc < 40 ){
-            console.log(5);
+        } else if (imc >= 35 && imc < 40) {
+            console.log(5); // Verificação do resultado
             return resultado.innerHTML = `<p>Seu IMC é de ${imc} (Obesidade grau 2)</p>`;
-        }else if(imc >= 40 ){
-            console.log(6);
+        } else if (imc >= 40) {
+            console.log(6); // Verificação do resultado
             return resultado.innerHTML = `<p>Seu IMC é de ${imc} (Obesidade grau 3)</p>`;
         };
     }
 
-    formulario.addEventListener("submit", enviar);
+    formulario.addEventListener("submit", enviar); // Verifica quando um evento acontece e quando acontecer aciona a função
 
 }
 
-
 imc();
+
+// inves de ter varios returns poderia se ter apenas um no final e nas condicionais modificar uma string que seria aplicada no return final
